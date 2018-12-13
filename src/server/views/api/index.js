8ad2991133b117cbe5a003e6f04dc253d534be21
Matt Shwery <mshwery@gmail.com>
2018-12-13 11:23:17 -0600

    add pause/resume feat for queue details page, redis docker image

--- src/server/views/api/index.js
+++ src/server/views/api/index.js
@@ -4,10 +4,15 @@ const jobRetry = require('./jobRetry');
 const jobRemove = require('./jobRemove');
 const bulkJobsRemove = require('./bulkJobsRemove');
 const bulkJobsRetry = require('./bulkJobsRetry');
+const queuePause = require('./queuePause');
+const queueResume = require('./queueResume');
 
 router.post('/queue/:queueHost/:queueName/job/bulk', bulkJobsRemove);
 router.patch('/queue/:queueHost/:queueName/job/bulk', bulkJobsRetry);
 router.patch('/queue/:queueHost/:queueName/job/:id', jobRetry);
 router.delete('/queue/:queueHost/:queueName/job/:id', jobRemove);
+
+router.post('/queue/:queueHost/:queueName/pause', queuePause);
+router.post('/queue/:queueHost/:queueName/resume', queueResume);
 
 module.exports = router;
